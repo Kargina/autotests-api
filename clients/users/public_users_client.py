c2580da4968832bd533dde5a3e2f61028d438ef8
Kargina <kargina.ks@gmail.com>
2026-09-08 22:16:47 +0300

    Add builders

--- clients/users/public_users_client.py
+++ clients/users/public_users_client.py
@@ -6,6 +6,17 @@ from clients.api_client import APIClient
 from clients.public_http_builder import get_public_http_client
 
 
+class User(TypedDict):
+    """
+    Описание структуры пользователя.
+    """
+    id: str
+    email: str
+    lastName: str
+    firstName: str
+    middleName: str
+
+
 class CreateUserRequestDict(TypedDict):
     """
     Описание структуры запроса на создание пользователя.
@@ -17,19 +28,30 @@ class CreateUserRequestDict(TypedDict):
     middleName: str
 
 
+class CreateUserResponseDict(TypedDict):
+    """
+    Описание структуры ответа создания пользователя.
+    """
+    user: User
+
+
 class PublicUsersClient(APIClient):
     """
-    Клиент для работы с публичными методами /api/v1/users
+    Клиент для работы с /api/v1/users
     """
 
     def create_user_api(self, request: CreateUserRequestDict) -> Response:
         """
         Метод создает пользователя.
 
-        :param request: Словарь с email, password, lastName, firstName и middleName
+        :param request: Словарь с email, password, lastName, firstName, middleName.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
         return self.post("/api/v1/users", json=request)
+
+    def create_user(self, request: CreateUserRequestDict) -> CreateUserResponseDict:
+        response = self.create_user_api(request)
+        return response.json()
 
 
 def get_public_users_client() -> PublicUsersClient:
